03a1c3c15775ca0e34eb8ab3edec46fdf4c90ac2
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-03-23 23:46:38 +0100

    Fix mistakes in js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 // 9.4 Loops - Exercise - tree
-   
+
 function drawTree (k) {
-	for (var i = 0; i < k; i++) { //pion
-		var star = '';            
-		for (var j = 0; j < i; j++) { // poziom               
-			star += '*';                  
-		}        
-	console.log(star);                   
-	}
+  for (var i = 0; i < k; i++) { //pion
+    var star = '';
+    for (var j = 0; j < i; j++) { // poziom
+      star += '*';
+    }
+  console.log(star);
+  }
 }
 
 drawTree(9);
-    
-function drawTreeMiddle (k) {    
-	for (var i = 0; i < k; i++) { //pion		 
-		var star = '';
-		var space = '';			 
-		for(var x = 0 ; x < 2*i-1 ; x++){				
-			star += '*';
-		}			 
-		for (var j = k; j > i; j--) { // poziom				
-			space += ' ';							   
-		}		
-		console.log(space+star+space+space+star);  		  
-	}
+
+function drawTreeMiddle (k) {
+  for (var i = 0; i < k; i++) { //pion
+    var star = '';
+    var space = '';
+    for(var x = 0 ; x < 2*i-1 ; x++){
+      star += '*';
+    }
+    for (var j = k; j > i; j--) { // poziom
+      space += ' ';
+    }
+  console.log(space+star+space+space+star);
+  }
 }
 
 drawTreeMiddle(5);
